68c6fa1c0b549a76ce5af7b6901529913fc9780e
Harish-s-24 <Harish.s@skiff.com>
2024-06-06 20:05:38 +0530

    Shopzone

---
+++ script.js
@@ -0,0 +1,11 @@
+var side=document.querySelector(".side")
+console.log(side)
+
+      function bar(){
+        side.style.left="0"
+      }
+
+     function cls(){
+        side.style.left="-30%"
+     }
+
